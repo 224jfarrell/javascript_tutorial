198227efe82faa727a437772e29b65f005a3af80
joseph farrell <224jfarrell@frhsd.com>
2023-10-02 09:30:44 -0400

    chapter 1 and 2 @ 17:21

---
+++ chapter2/sandbox.js
@@ -0,0 +1,34 @@
+//initial command
+alert('hello world');
+
+//console.log
+console.log(1);
+console.log(2);
+
+//let variables
+let age = 17;
+let year = 2023;
+console.log(age, year);
+age = 17.5;
+console.log(age);
+
+//const variables
+const points = 100;
+console.log(points);
+
+//var variables
+var score = 75;
+console.log(score);
+
+//single line comment
+/*  multi
+    line
+    comment */
+// control forwardslash
+
+//data types
+var number1 = 1;
+var number2 = 2;
+var number3 = 3;
+var number4 = 100;
+var number5 = 3.14;
